c82601794b52f2a77814854eec458a5a277a45c3
ZL <9095663212@qq.com>
2023-03-27 15:49:32 +0800

    22

--- deploy.sh
+++ deploy.sh
@@ -17,8 +17,8 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/MRLesile/WebBook.git master:gh-pages
+git push -f https://[Token]@github.com/MRLesile/WebBook.git main:gh-pages
 cd -
